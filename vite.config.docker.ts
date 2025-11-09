c581684d5a8f3489854dea4572ed944475384020
yaffalhakim1 <yafialhakim64@gmail.com>
2025-11-09 17:56:39 +0700

    feat: add MDX plugin to Vite configuration for enhanced documentation support

--- vite.config.docker.ts
+++ vite.config.docker.ts
@@ -2,7 +2,9 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import mdx from 'fumadocs-mdx/vite';
+import * as MdxConfig from './source.config';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), mdx(MdxConfig)],
 });
